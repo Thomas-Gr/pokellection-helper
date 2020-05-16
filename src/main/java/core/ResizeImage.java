ac9ca37dcb9437d1112ce7fc8f9715f621155289
Thomas-Gr <thomas.grilletta@gmail.com>
2020-05-16 23:54:47 +0900

    Revert ResizeImage

--- src/main/java/core/ResizeImage.java
+++ src/main/java/core/ResizeImage.java
@@ -9,48 +9,42 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class ResizeImage {
 
     public static void main(String [] args) throws IOException {
-        //resizeImage("series", 40, false);
-        resizeImage("cards", 300, true);
+        resizeImage("series", 40, false);
+        resizeImage("cards", 250, true);
     }
 
     private static void resizeImage(String folderName, int newSize, boolean widthFirst) throws IOException {
-        final File folder = new File("/Users/grillett/dev/card-assembler/website/images/full/");
+        final File folder = new File(String.format("./out/images/%s/", folderName));
 
-        Arrays.stream(requireNonNull(folder.listFiles())).parallel().forEach(file -> {
-            try {
-                if (!file.getName().equals(".DS_Store")) {
+        for (File file : requireNonNull(folder.listFiles())) {
+            if (file.getName().equals(".DS_Store")) continue;
 
-                    BufferedImage originalImage = ImageIO.read(file.getAbsoluteFile());
-                    int height = originalImage.getHeight();
-                    int width = originalImage.getWidth();
+            BufferedImage originalImage = ImageIO.read(file.getAbsoluteFile());
+            int height = originalImage.getHeight();
+            int width = originalImage.getWidth();
 
-                    int newWidth, newHeight;
+            int newWidth, newHeight;
 
-                    if (width > height || widthFirst) {
-                        newWidth = newSize;
-                        newHeight = height * newSize / width;
-                    } else {
-                        newWidth = height * newSize / width;
-                        newHeight = newSize;
-                    }
-
-                    BufferedImage scaledImage = resize(originalImage, ULTRA_QUALITY, FIT_EXACT, newWidth, newHeight);
-
-                    String extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
-
-                    ImageIO.write(
-                        scaledImage,
-                        extension,
-                        new File(String.format("/Users/grillett/dev/card-assembler/website/images/min_m/%s", file.getName())));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (width > height || widthFirst) {
+                newWidth = newSize;
+                newHeight = height * newSize / width;
+            } else {
+                newWidth = height * newSize / width;
+                newHeight = newSize;
             }
-        });
+
+            BufferedImage scaledImage = resize(originalImage, ULTRA_QUALITY, FIT_EXACT, newWidth, newHeight);
+
+            String extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+
+            ImageIO.write(
+                    scaledImage,
+                    extension,
+                    new File(String.format("./out/images/resized_%s/%s", folderName, file.getName())));
+        }
     }
 }
